4ba6851af8642309642d8b99fb4e1b26fb0da389
benbarker04 <>
2023-10-09 12:45:22 +0000

    make const for buttons, player score, bot score, player image , bot image and choices

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const buttons = document.getElementsByClassName('choices')
+const playerScore = document.getElementById('player-score')
+const botScore = document.getElementById('bots-score')
+const playerImage = document.getElementById('player-image')
+const botImage = document.getElementById('bot-image')
+const choices = ['rock' ,'paper' ,'scissors']
