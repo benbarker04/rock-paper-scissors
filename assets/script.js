b3d0b16057c053b736b1c62ab34371203c5ee353
benbarker04 <>
2023-10-09 13:36:39 +0000

    add event listener for when buttons are clicked and add code for players choice

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,20 @@
-const buttons = document.getElementsByClassName('choices')
+const buttons = document.getElementsByClassName('buttons')
 const playerScore = document.getElementById('player-score')
 const botScore = document.getElementById('bots-score')
 const playerImage = document.getElementById('player-image')
 const botImage = document.getElementById('bot-image')
 const choices = ['rock' ,'paper' ,'scissors']
+
+/*Event listener for buttons*/
+for (let button of buttons) {
+    button.addEventListener('click', function(){
+        let playerChoice = this.getAttribute('data-choice');
+        playGame(playerChoice)
+    } )
+}
+
+function playGame(playerChoice){
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice]
+
+}
