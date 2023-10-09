21789b9d43334a714fac91d2d732b59b1564cbe0
benbarker04 <>
2023-10-09 16:04:55 +0000

    add function that adds to player and computer score depending on who wone

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const buttons = document.getElementsByClassName('buttons')
-const playerScore = document.getElementById('player-score')
-const botScore = document.getElementById('bots-score')
+/*const playerScore = document.getElementById('player-score')*/
+/*const botScore = document.getElementById('bots-score')*/
 const playerImage = document.getElementById('player-image')
 const botImage = document.getElementById('bot-image')
 const choices = ['rock' ,'paper' ,'scissors']
@@ -29,27 +29,37 @@ function playGame(playerChoice){
 }
 
 
-/*Check winner*/
+/*Check winner and add to score*/
 
 function checkWinner(playerChoice, botChoice) {
     if (playerChoice == botChoice) {
         console.log('tie');
     } else if (playerChoice == "rock" && botChoice == "scissors") {
+        let oldBotScore = parseInt(document.getElementById('bots-score').innerText);
+        document.getElementById('bots-score').innerText = ++oldBotScore
         console.log('bot wins');
     } else if (playerChoice == "scissors" && botChoice == "paper") {
+        oldBotScore = parseInt(document.getElementById('bots-score').innerText);
+        document.getElementById('bots-score').innerText = ++oldBotScore
         console.log('bot wins');
     }else if(playerChoice == "paper" && botChoice == "rock"){
+        oldBotScore = parseInt(document.getElementById('bots-score').innerText);
+        document.getElementById('bots-score').innerText = ++oldBotScore
             console.log('bot wins');
         }
     
 
     if (botChoice == "rock" && playerChoice == "scissors"){
+        let oldPlayerScore = parseInt(document.getElementById('player-score').innerText);
+        document.getElementById('player-score').innerText = ++oldPlayerScore
         console.log('player wins');
     }else if(botChoice == "scissors" && playerChoice == "paper"){
+        oldPlayerScore = parseInt(document.getElementById('player-score').innerText);
+        document.getElementById('player-score').innerText = ++oldPlayerScore
         console.log('player wins');
     }else if(botChoice == "paper" && playerChoice == "rock"){
+        oldPlayerScore = parseInt(document.getElementById('player-score').innerText);
+        document.getElementById('player-score').innerText = ++oldPlayerScore
         console.log('player wins');
     }
 }
-
-
